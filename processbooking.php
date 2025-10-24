e52cfad7aa69e32e195ce9374c418f1d9b73c59a
Kenzie Nguyen <106223201@student.swin.edu.au>
2025-10-24 09:32:13 +0700

    Final commit

--- processbooking.php
+++ processbooking.php
@@ -8,19 +8,78 @@
 <body>
     <h1>Rohirrim Tour Booking Confirmation</h1>
     <?php
-    $fname = $_POST["firstname"];
-    $lname = $_POST["lastname"];
-    $age = $_POST["age"];
 
-    $email = $_POST["email"];
+    // Check if the form has been submitted via POST //
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Extract the values from the form //
+    $firstName = isset($_POST["firstname"]) ? htmlspecialchars($_POST["firstname"]) : "";
+    $lastName = isset($_POST["lastname"]) ? htmlspecialchars($_POST["lastname"]) : "";
+    $age = isset($_POST["age"]) ? htmlspecialchars($_POST["age"]) : "";
+
+    // Get species value and convert into text //
+    $speciesValue = isset($_POST["species"]) ? $_POST["species"] : "";
+    $speciesMap = array(
+        "M" => "Human", 
+        "D" => "Dwarf", 
+        "E" => "Elf", 
+        "H" => "Hobbit"
+    );
+    $species = isset($speciesMap[$speciesValue]) ? $speciesMap[$speciesValue] : "Unknown";
+
+    // Get food preferences //
+    $food = isset($_POST["food"]) ? htmlspecialchars($_POST["food"]) : "";
+    $foodMap = array(
+        "none" => "None",
+        "lembas" => "Lembas",
+        "mushrooms" => "Mushrooms",
+        "ent" => "Ent Draft",
+        "cram" => "Cram"
+    );
+    $meal = isset($foodMap[$food]) ? $foodMap[$food] : ucfirst($food);
+
+    // Get number of travellers //
+    $partySize = isset($_POST["partysize"]) ? htmlspecialchars($_POST["partysize"]) : "";
+
+    // Get booking date //
+    $bookDay = isset($_POST["bookday"]) ? htmlspecialchars($_POST["bookday"]) : "";
+
+    // Get selected accommodation and tours //
+
+    $toursArray = array();
+
+    if (isset($_POST["accom"])) {
+        $toursArray[] = "Accommodation";
+    }
+    if (isset($_POST["4day"])) {
+        $toursArray[] = "4 Day Tour";
+    }
+    if (isset($_POST["10day"])) {
+        $toursArray[] = "10 Day Tour";
+    }
+
+    if (sizeof($toursArray) > 0) {
+        $tours = implode(", ", $toursArray);
+     } else {
+        $tours = "None";
+     }
     
-
-    // if(isset($_POST["fname"]))
-    //     $fname = $_POST["firstname"];
-
-    echo "<p>Welcome $fname $lname !</p>";
+    // Display the results //
+    echo "<p>Welcome $firstName $lastName!</p>";
     echo "<p>Age: $age</p>";
-    echo "You are now booked on the ";
+    echo "<p>Species: $species</p>";
+    echo "<p>Tours: $tours</p>";
+    echo "<p>Meal: $meal</p>";
+    echo "<p>Party Size: $partySize</p>";
+    if (!empty($bookDay)) {
+        echo "<p>Booking Date: $bookDay</p>";
+    }
+    echo "<p></p>";
+}
+
+else {
+    echo "<p>Please fill in the form and submit it.</p>";
+}
     ?>
     
 </body>
